e3fa09c123ff98ce1ea50f2de2701607af292797
yangyibo059 <72655783+yang-059@users.noreply.github.com>
2020-11-09 13:22:34 +0800

    Add files via upload

--- boYanJiuShengZhuJiao/Test.java
+++ boYanJiuShengZhuJiao/Test.java
@@ -4,13 +4,37 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		BoShiYanJiuSheng bj1=new BoShiYanJiuSheng("李四", 26, "男",6000,500,4600,0); 
-		BoShiYanJiuSheng bj2=new BoShiYanJiuSheng("王五", 27, "女",11000,200,5000,4000);
+		BoShiYanJiuSheng bj1=new BoShiYanJiuSheng("李四", 26, "男",6000,500,4600); 
+		BoShiYanJiuSheng bj2=new BoShiYanJiuSheng("王五", 27, "女",11000,200,5000);
 	    bj1.setSalary(6000, 500);
 	    bj1.getTuition(5000);
+	    bj1.queryTuition(3300);
 	    bj1.Taxable();
+	    bj1.getbanlance();
 	    bj1.toString();
 	    
+	    bj2.setSalary(6000, 500);
+	    bj2.getTuition(5000);
+	    bj2.queryTuition(10000);
+	    bj2.Taxable();
+	    bj2.toString();
+	    
+	    
+	    //输出内容
+	    
+	    System.out.println(bj1.toString());
+	    System.out.println();
+	    bj1.gettax();
+	    bj1.getSum();
+	    bj1.Sum();
+	    System.out.println();
+	    
+	    System.out.println(bj2.toString());
+	    System.out.println();
+	    bj2.gettax();
+	    bj2.getSum();
+	    bj2.Sum();
+	    System.out.println();
 	}	
 }
 
--- boYanJiuShengZhuJiao/ZhuJiao.java
+++ boYanJiuShengZhuJiao/ZhuJiao.java
@@ -2,6 +2,6 @@ package boYanJiuShengZhuJiao;
 
 public interface ZhuJiao {
 	float setSalary(float x,float z);    //发放薪水
-	float querySalary(float y);          //查询薪水
+	double querySalary(double y);          //查询薪水
 	
 }
